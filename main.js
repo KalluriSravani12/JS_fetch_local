bd7db264cf83e03f95d19d51ba6798eb2efb19dd
Kallurisravani12 <KalluriSravani12>
2023-04-18 11:50:39 +0200

    My first Commit

---
+++ main.js
@@ -0,0 +1,45 @@
+/*Different ways of resding data from a source*/
+
+//Promise chaining
+function readFilePromise(){
+    fetch('customers.json')
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Fetch error: ' + response.status);
+        }
+        return response.json();
+    })
+    .then(customers => {
+        generateHTML(customers);
+
+
+    })
+    .catch(error =>{
+        console.log('Error: '+ error)
+
+    });
+
+
+}
+
+// Render HTML
+ function generateHTML(customers){
+    let html =''; // IN ORDER TO MAKE THE HTML EMPTY AT THE STARING
+    for (let customer of customers){
+        html += `
+            <h3>${customer.firstName} ${customer.lastName}</h3>
+            <p> ${customer.firstName} works at ${customer.companyName}</p>
+        
+        
+        
+        `;
+    }
+        html +='<hr>';
+        let customerDiv = document.createElement('div');
+        customerDiv.innerHTML= html;
+        document.querySelector('body').append(customerDiv);
+
+
+    }
+
+ readFilePromise();
